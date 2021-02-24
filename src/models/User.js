c8572bd55332332f4e89de74a4965d29b0a3db94
Nicolas <niicolas.caro@gmail.com>
2021-02-24 03:59:36 -0300

    update User model. Encrypt password and validate
    * Update attributes: now camelCase
    * Add function for validPassword
    * password exclusion on returnþ

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,10 @@
+const { hashSync } = require("bcrypt");
+
 module.exports = function (sequelize, DataTypes) {
   const User = sequelize.define(
     "User",
     {
-      Email: {
+      email: {
         type: DataTypes.STRING(70),
         allowNull: false,
         unique: true,
@@ -10,22 +12,41 @@ module.exports = function (sequelize, DataTypes) {
           isEmail: true,
         },
       },
-      Name: {
+      name: {
         type: DataTypes.STRING(60),
         allowNull: false,
       },
-      Password: {
+      password: {
         type: DataTypes.STRING(60),
         allowNull: false,
       },
     },
     {
       timestamps: false,
+      instanceMethods: {
+        validPassword(password) {
+          return bcrypt.compareSync(password, this.password);
+        }
+      },
+      defaultScope: {
+        attributes: { exclude: ["password"] },
+      }
     }
   );
 
   User.associate = function (models) {
     User.hasMany(models.Operation, { as: "Operations" });
+  };
+
+  User.beforeCreate((user, options) => {
+    const hashedPassword = hashSync(user.password, 10);
+    user.password = hashedPassword;
+  });
+
+  User.prototype.toJSON = function () {
+    var values = Object.assign({}, this.get());
+    delete values.password;
+    return values;
   };
 
   return User;
